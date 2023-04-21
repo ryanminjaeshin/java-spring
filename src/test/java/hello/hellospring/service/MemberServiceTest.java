929e1962570f28e2dd921bbdf648f2bdba3a960f
Ryan Shin <ryanminjaeshin@gmail.com>
2023-04-21 22:34:12 +0900

    add test

---
+++ src/test/java/hello/hellospring/service/MemberServiceTest.java
@@ -0,0 +1,48 @@
+package hello.hellospring.service;
+
+import hello.hellospring.domain.Member;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class MemberServiceTest {
+
+    MemberService memberService = new MemberService();
+
+    @Test
+    void 회원가입(){
+        //given
+        Member member = new Member();
+        member.setName("hello");
+
+
+        //when
+            // 저장한것을 리포지토리에서 꺼내야함
+        Long saveId = memberService.join(member);
+
+        //then
+        Member findMember = memberService.findOne(saveId).get();
+        // static import => option + enter
+        assertThat(member.getName()).isEqualTo(findMember.getName());
+    }
+
+    @Test
+    public void 중복_회원_예외(){
+        //given
+        Member member1 = new Member();
+        member1.setName("Spring");
+
+        Member member2 = new Member();
+        member2.setName("Spring");
+
+        //when
+        memberService.join(member1);
+        IllegalStateException e = assertThrows(IllegalStateException.class, () -> memberService.join(member2));
+        assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다");
+        //then
+    }
+}
