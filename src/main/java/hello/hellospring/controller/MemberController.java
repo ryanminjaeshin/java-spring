2f1e0f9c304e05261304cc8d12273faf6e8c529c
Ryan Shin <ryanminjaeshin@gmail.com>
2023-04-22 22:16:20 +0900

    add notes'

--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import javax.swing.plaf.metal.MetalMenuBarUI;
 
 // 위 Controller 어노테이션을 보고 스프링이 뜰때 멤머 컨트롤러 객체 생성 => 스프링 컨테이너에서 스피링빈이 관리된다고 말함
+// Controller - 외부요청 받고, Service에서 비지니스 로직을 만들고, Repository에서 데이터를 저장
 @Controller
 public class MemberController {
 
