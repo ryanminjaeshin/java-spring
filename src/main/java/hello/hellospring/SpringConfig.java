d65dc40c5f9ed9c32187a1d0df43f797dd8d5cb3
Ryan Shin <ryanminjaeshin@gmail.com>
2023-05-05 16:41:56 +0900

    WIP jdbc template

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -1,25 +1,48 @@
 package hello.hellospring;
 
+//import hello.hellospring.repository.JdbcTemplateMemberRepository;
+import hello.hellospring.repository.JdbcTemplateMemberRepository;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
 import hello.hellospring.service.MemberService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-// Spring bean 자바코드로 직접 등록 방식
+import javax.sql.DataSource;
+
+//// Spring bean 자바코드로 직접 등록 방식
+//@Configuration
+//public class SpringConfig {
+//
+//    //구동시 memberService와 memberRepository 스프링빈에 등록
+//    @Bean
+//    public MemberService memberService(){
+//        return new MemberService(memberRepository());
+//    }
+//
+//    @Bean
+//    public MemberRepository memberRepository(){
+//        // 데이터베이스 변경시, MemoryMemberRepository 명만 변경하면 됨.
+//        // 구현체 (MemoryMemberRepository) 등록
+//        return new MemoryMemberRepository();
+//    }
+//}
+
+import javax.sql.DataSource;
 @Configuration
 public class SpringConfig {
-
-    //구동시 memberService와 memberRepository 스프링빈에 등록
+    private final DataSource dataSource;
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
     @Bean
-    public MemberService memberService(){
+    public MemberService memberService() {
         return new MemberService(memberRepository());
     }
-
     @Bean
-    public MemberRepository memberRepository(){
-        // 데이터베이스 변경시, MemoryMemberRepository 명만 변경하면 됨.
-        // 구현체 (MemoryMemberRepository) 등록
-        return new MemoryMemberRepository();
+    public MemberRepository memberRepository() {
+// return new MemoryMemberRepository();
+// return new JdbcMemberRepository(dataSource);
+        return new JdbcTemplateMemberRepository(dataSource);
     }
 }
